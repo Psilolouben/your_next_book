2d92031db3d3086cc40807a156817120441f3193
Markos Rigas <markos.rigas@persado.com>
2024-09-19 16:40:56 +0300

    Include shelf filtering

--- proposal.go
+++ proposal.go
@@ -2,37 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-resty/resty/v2"
+	//"github.com/go-resty/resty/v2"
 	"encoding/csv"
 	"os"
 	"log"
+	"strings"
 )
 
-func main() {
-	client := resty.New()
-	csv_file_path := "./goodreads_library_export.csv"
+func filteredByShelf(books [][]string, shelfName string)(bks [][]string){
+	for _, bk := range books {
+		if Contains(bk[16], shelfName) {
+			bks = append(bks, bk)
+		}
+	}
+	return bks
+}
 
-	f, err := os.Open(csv_file_path)
+func csvData(filePath string)(records [][]string) {
+	f, err := os.Open(filePath)
 	if err != nil {
         log.Fatal("Unable to read input file " + filePath, err)
     }
-    defer f.Close()
 
-	r := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
+	csvReader := csv.NewReader(f)
+	records, err = csvReader.ReadAll()
     if err != nil {
         log.Fatal("Unable to parse file as CSV for " + filePath, err)
     }
 
-	fmt.Println(records)
+	f.Close()
+	return
+}
 
-	resp, err := client.R().
-		EnableTrace().
-		Get("https://httpbin.org/get")
+func main() {
+	//client := resty.New()
+	
+	r := csvData("./goodreads_library_export.csv")
+	
 
-	if err == nil {
-		fmt.Println(resp.Time())
-	} else {
-		fmt.Println(err)
-	}
+	r = filteredByShelf(r, "looking-for")
+	fmt.Println(r)
+
+	//resp, err := client.R().
+	//	EnableTrace().
+	//	Get("https://httpbin.org/get")
+//
+	//if err == nil {
+	//	fmt.Println(resp.Time())
+	//} else {
+	//	fmt.Println(err)
+	//}
 }
