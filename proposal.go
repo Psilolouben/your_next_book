68c7fab30092cd0bb04b22fab347efffde7c07ed
Markos Rigas <markos.rigas@persado.com>
2024-09-19 12:59:46 +0300

    Add csv

--- proposal.go
+++ proposal.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 import "github.com/go-resty/resty/v2"
+import "encoding/csv"
+import "strings"
 
 func main() {
 	client := resty.New()
+	//csv_file_path := "./goodreads_library_export.csv"
 
+	in := `first_name,last_name,username
+"Rob","Pike",rob
+Ken,Thompson,ken
+"Robert","Griesemer","gri"
+`
+	r := csv.NewReader(strings.NewReader(in))
+
+	fmt.Println(r)
 	resp, err := client.R().
 		EnableTrace().
 		Get("https://httpbin.org/get")
